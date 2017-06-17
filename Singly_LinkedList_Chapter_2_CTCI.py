1837a5cc3015046a5c6be2bf79d99a40b1467e13
Daricus_Duncan <drdunkan15@gmail.com>
2017-06-17 02:11:38 -0500

    Working on removing dupes from LL
    remove Duplicates func into class for LL

--- Singly_LinkedList_Chapter_2_CTCI.py
+++ Singly_LinkedList_Chapter_2_CTCI.py
@@ -33,7 +33,74 @@ class LinkedList(object):
     def getSize(self):
 
         return (self.size)
+
+    def removeByPosition(self,position):
+        current = self.head
+        count = 1
+
+        if not (self.head):
+            return("Add elements to the LinkedList first")
+        elif(position == 1):
+            origHead = self.head
+            newHead = self.head
+            self.head = newHead.next
+        elif(position >= 1):
+            while(count != (position-1)):
+                current = current.next
+                count+=1
+        current.next = current.next.next
+        self.size -= 1
+
+
+    def removeDuplicates(self):
+        temp = self.head
+        temp2 = self.head.next
+        count = 0
+
+        if(self.size == 0):
+            return("Add elements to LinkedList")
+        elif(self.size == 1):
+            return("No duplicates")
+        elif(self.size == 2):
+            if(temp.data == temp2.data):
+                temp.next = temp.next.next
+        else:
             
+            while not (temp):
+                temp2 = temp.next
+                while not (temp2):
+                    if (temp.data == temp2.data):
+                        temp.next = temp.next.next
+                    
+                temp2 = temp2.next
+            temp = temp.next
+            
+            
+        
+            
+        
+                
+                
+                
+                
+             
+             
+        
+
+
+
+linked = LinkedList()
+linked.append(1)
+linked.append(2)
+linked.append(3)
+linked.append(4)
+linked.append(2)
+linked.append(3)
+linked.append(4)
+
+
+# 1 2 3 4
+# 0 1 2 3
 
     
         
